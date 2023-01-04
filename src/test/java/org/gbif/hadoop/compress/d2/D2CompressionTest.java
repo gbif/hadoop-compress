c0beb8caa47389fa76e289eea089cb90d9e54a62
fmendezh <federicomh@gmail.com>
2023-01-04 17:19:06 +0100

    formatting https://github.com/gbif/pipelines/issues/843

--- src/test/java/org/gbif/hadoop/compress/d2/D2CompressionTest.java
+++ src/test/java/org/gbif/hadoop/compress/d2/D2CompressionTest.java
@@ -1,3 +1,16 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.gbif.hadoop.compress.d2;
 
 import java.io.BufferedOutputStream;
@@ -5,14 +18,15 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.List;
-import java.security.SecureRandom;
+
+import org.junit.Assert;
+import org.junit.Test;
 
 import com.google.common.collect.Lists;
 import com.google.common.io.Files;
-import org.junit.Assert;
-import org.junit.Test;
 
 /**
  * Tests that demonstrate compression and decompression round tripping.
